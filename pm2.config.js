f8722fa9f8eaa7385b981a6d8f3b65c07fbbb72f
Daniela <danielamij251@gmail.com>
2024-03-05 10:30:54 -0300

    pm2 file

---
+++ pm2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: "mi-aplicacion",
+        script: "npm run dev",
+        env: {
+          NODE_ENV: "development",
+        },
+      },
+    ],
+  };
